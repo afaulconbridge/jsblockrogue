7f0a5c0966ac76594c74cda4a05f6b98236edd1d
Adam Faulconbridge <afaulconbridge@googlemail.com>
2015-08-27 17:03:54 +0100

    expanded keymappings

--- game.js
+++ game.js
@@ -39,50 +39,12 @@ var Game = {
                 this.map[key] = ".";
                 freeCells.push(key);
             }
+            //doors are just marked as walls at this point
         };
-        
-        
+                
         var dugmap = digger.create(digCallback.bind(this));
         
-        //at this point we have some rooms and some corridors
-        //go through everything and put it in the map
-        for (var y = 0 ; y  < ROT.DEFAULT_HEIGHT; y++) {
-            for (var x = 0 ; x < ROT.DEFAULT_WIDTH; x++) {
-                this.map[x+","+y] = '#';
-            }
-        }
-        
-        var rooms = dugmap.getRooms();
-        for (var i = 0; i < rooms.length; i++) {
-            var room = rooms[i];
-            for (var y = room.getTop() ; y  <= room.getBottom(); y++) {
-                for (var x = room.getLeft() ; x  <= room.getRight(); x++) {
-                    this.map[x+","+y] = '.';
-                }
-            }    
-            //access doors as private variables
-            for (var j = 0; j < room._doors.length; j++) {
-                var door = room._doors[j];
-                console.log("Door "+door);
-                this.map[door] = '+';
-            }
-        }
-        var corridors = dugmap.getCorridors(); 
-        for (var i = 0; i < corridors.length; i++) {
-            var corridor = corridors[i];
-            //have to access this as private variables
-            var minY = Math.min(corridor._startY, corridor._endY);
-            var maxY = Math.max(corridor._startY, corridor._endY);
-            var minX = Math.min(corridor._startX, corridor._endX);
-            var maxX = Math.max(corridor._startX, corridor._endX);
-            for (var y = minY ; y <= maxY; y++) {
-                for (var x = minX ; x <= maxX; x++) {
-                    this.map[x+","+y] = '.';
-                }
-            }    
-        }
-        
-        this._createPlayer(freeCells);
+        this._createPlaer(freeCells);
     },
     
     _createPlayer: function(freeCells) {
@@ -99,7 +61,7 @@ var Game = {
             var parts = key.split(",");
             var x = parseInt(parts[0]);
             var y = parseInt(parts[1]);
-            this.display.draw(x, y, this.map[key]);
+            this.display.draw(x, y, this.map[key], "#ff0");
         }
         this.display.draw(this.player._x, this.player._y, "@", "#ff0");
     }
@@ -119,25 +81,38 @@ Player.prototype.act = function() {
 Player.prototype.handleEvent = function(e) {
     
     var keyMap = {};
-    keyMap[38] = 0;
-    keyMap[33] = 1;
-    keyMap[39] = 2;
-    keyMap[34] = 3;
-    keyMap[40] = 4;
-    keyMap[35] = 5;
-    keyMap[37] = 6;
-    keyMap[36] = 7;
+    //numpad directions
+    keyMap[ROT.VK_UP] = 0;
+    keyMap[ROT.VK_PAGE_UP] = 1;
+    keyMap[ROT.VK_RIGHT] = 2;
+    keyMap[ROT.VK_PAGE_DOWN] = 3;
+    keyMap[ROT.VK_DOWN] = 4;
+    keyMap[ROT.VK_END] = 5;
+    keyMap[ROT.VK_LEFT] = 6;
+    keyMap[ROT.VK_HOME] = 7;
+    keyMap[ROT.VK_NUMPAD8] = 0;
+    keyMap[ROT.VK_NUMPAD9] = 1;
+    keyMap[ROT.VK_NUMPAD6] = 2;
+    keyMap[ROT.VK_NUMPAD3] = 3;
+    keyMap[ROT.VK_NUMPAD2] = 4;
+    keyMap[ROT.VK_NUMPAD1] = 5;
+    keyMap[ROT.VK_NUMPAD4] = 6;
+    keyMap[ROT.VK_NUMPAD7] = 7;
 
     var code = e.keyCode;
     /* one of numpad directions? */
-    if (!(code in keyMap)) { return; }
+    if (!(code in keyMap)) { 
+        return; 
+    }
 
     /* is there a free space? */
     var dir = ROT.DIRS[8][keyMap[code]];
     var newX = this._x + dir[0];
     var newY = this._y + dir[1];
     var newKey = newX + "," + newY;
-    if (!(newKey in Game.map)) { return; }
+    if (!(newKey in Game.map) ||  (Game.map[newKey] === "#")) { 
+        return; 
+    }
 
     this._x = newX;
     this._y = newY;
